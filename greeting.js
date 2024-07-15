1f5abb888a76f08e6c6d8ada764ab772526af532
Pop Lucas <lucaspop5511@gmail.com>
2024-07-15 15:18:54 +0300

    almost finished the landing page

--- greeting.js
+++ greeting.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const startButton = document.getElementById('start-button');
     const nameForm = document.getElementById('name-form');
     const pianoSection = document.getElementById('piano-section');
     const usernameInput = document.getElementById('username');
+    const logoSection = document.getElementById('logo-section');
+    const logoIMG = document.getElementById('logoIMG');
+    const earTraining = document.getElementById('earTraining');
+
+    startButton.addEventListener('click', function () {
+        startButton.style.display = 'none';
+        logoSection.classList.add('move-up');
+        logoIMG.classList.add('shrink');
+        earTraining.style.opacity = '0';
+
+        logoIMG.addEventListener('animationend', function () {
+            nameForm.style.display = 'block';
+        }, { once: true });
+    });
 
     nameForm.addEventListener('submit', function (event) {
         event.preventDefault();
